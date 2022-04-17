207f345e89607255a32f2917b3a6d1cdce7a0818
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-04-17 15:38:48 +0900

    add composer exit
    q
    ;q
    q;
    
    exit
    exit;
    
    q
    ;

--- composerTest/vendor/composer/autoload_real.php
+++ composerTest/vendor/composer/autoload_real.php
@@ -50,6 +50,24 @@ class ComposerAutoloaderInit9bf777e9b944cd4509ad84a243636336
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit9bf777e9b944cd4509ad84a243636336::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire9bf777e9b944cd4509ad84a243636336($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire9bf777e9b944cd4509ad84a243636336($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
